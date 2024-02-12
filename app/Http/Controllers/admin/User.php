41d0594291b3ded2ff21ad03e36917457ccfad93
Angel Jean Pierre Calderon Mantilla <angelitojpcmantilla24@gmail.com>
2024-02-12 09:47:38 -0500

    Agregada dependencia "tymon/jwt-auth" a composer.json

---
+++ app/Http/Controllers/admin/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Controller implements JWTSubject
+{
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
